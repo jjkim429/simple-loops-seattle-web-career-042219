ecf67273258b0c965ac80fc98100c48ee9facda6
Jennifer Kim <jjkim429@gmail.com>
2019-04-05 22:15:11 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -6,7 +6,6 @@ end
 
 def times_iterator(number_of_times)
   7.times do
-    phrase = "Welcome to Flatiron School's Web Development Course!"
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
@@ -17,8 +16,6 @@ def while_iterator(number_of_times)
     puts "Welcome to Flatiron School's Web Development Course!"
     counter += 1
   end
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
 end
 
 def until_iterator(number_of_times)
