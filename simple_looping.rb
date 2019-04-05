380f0fbecf9722786e5303c1523e25eb919b5fb0
Jennifer Kim <jjkim429@gmail.com>
2019-04-05 22:22:09 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,8 @@ def loop_iterator(number_of_times)
   loop do
     counter +=1
     puts "Welcome to Flatiron School's Web Development Course!"
-    break if 
+    break if counter > 7
+  end
 end
 
 def times_iterator(number_of_times)
